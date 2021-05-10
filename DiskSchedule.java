b298dd0d44b7bb84bb776f5418cb43b19711d82c
Amr Abedo <mouraa@Amrs-MBP.home>
2021-05-10 14:14:22 -0400

    Added proof of work

--- DiskSchedule.java
+++ DiskSchedule.java
@@ -3,25 +3,39 @@ import java.util.*;
 public class DiskSchedule {
 	public static void main(String[] args) {
 
-		int head = 100;	
-		int requests[] = {4078, 153, 2819, 3294, 1433, 211, 1594, 2004, 2335, 2007, 771, 1043, 3950, 2784, 1881,
-	        2931, 3599, 1245, 4086, 520, 3901, 2866, 947, 3794, 2353, 3970, 3948, 1815, 4621, 372, 2684, 3088, 
-	        827, 3126, 2083, 584, 4420, 1294, 917, 2881, 3659, 2868, 100, 1581, 4581, 1664, 1001, 1213, 3439, 4706};
+		int head = 53;	
+		// int requests[] = {4078, 153, 2819, 3294, 1433, 211, 1594, 2004, 2335, 2007, 771, 1043, 3950, 2784, 1881,
+	 //        2931, 3599, 1245, 4086, 520, 3901, 2866, 947, 3794, 2353, 3970, 3948, 1815, 4621, 372, 2684, 3088, 
+	 //        827, 3126, 2083, 584, 4420, 1294, 917, 2881, 3659, 2868, 100, 1581, 4581, 1664, 1001, 1213, 3439, 4706};
+
+		int requests[] = {98,183,37,122,14,124,65,67};
+		int max = 199; 
 
 	    System.out.println("FCFS: " + FCFS(requests,head));
+	    System.out.println();
 	    System.out.println("SSTF: " + SSTF(requests,head));
-	    System.out.println("SCAN: " + SCAN(requests,head));
-        System.out.println("C-SCAN: " + C_SCAN(requests,head));
+	    System.out.println();
+	    System.out.println("SCAN: " + SCAN(requests,head,max));
+	    System.out.println();		
+        System.out.println("C-SCAN: " + C_SCAN(requests,head,max));
+        System.out.println();
         System.out.println("LOOK: " + LOOK(requests,head));
+        System.out.println();
         System.out.println("C-LOOK: " + C_LOOK(requests,head));
+        System.out.println();
 	}
 
 	public static int FCFS(int[] requests, int head) {
 		int head_movements = 0;
         for(int i = 0; i < requests.length; i++) {
-                head_movements += Math.abs(head - requests[i]);
-                head = requests[i];
+        	if(i == requests.length - 1) 
+        		System.out.print("(|" + head + " - " + requests[i] + "|)");	
+        	else 
+        		System.out.print("(|" + head + " - " + requests[i] + "|) + ");
+            head_movements += Math.abs(head - requests[i]);
+            head = requests[i];
         }
+        System.out.println();
         return head_movements;
 	}
 
@@ -45,16 +59,19 @@ public class DiskSchedule {
                 }
             }
 
+            System.out.print(shortest + " + ");
+
             head_movements += shortest;
             head = temp.get(position);
             temp.remove(position);
         }
 
+        System.out.println();
         return head_movements;
     }
 
     //IF DIRECTION IS TO THE RIGHT
-    public static int SCAN(int [] requests, int head) {
+    public static int SCAN(int [] requests, int head, int max) {
     	int head_movements = 0;
         int position = 0;
 		ArrayList <Integer> temp = new ArrayList<Integer>();
@@ -74,21 +91,27 @@ public class DiskSchedule {
         
         //going to the right side first
         for(int i = position; i < temp.size(); i++) {
+        	System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
             head_movements += Math.abs(temp.get(i) - head);
             head = temp.get(i);
         }
 
-        head_movements += 4999 - head;
+        head_movements += max - head;
+        System.out.print("(|" + max + " - " + head + "|) + ");
 
         //iterate backwards to get the remaining requests (left side)
         for(int i = position - 1; i >= 0; i--) {
+        	if(i == 0)
+        		System.out.print("(|" + head + " - " + temp.get(i) + "|)");
+        	else
+        		System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
             head_movements += Math.abs(temp.get(i) - head);
         }
-
+        System.out.println();
     	return head_movements;
     }
 
-    //IF DIRECTION IS TO THE LEFT
+    // IF DIRECTION IS TO THE LEFT
     // public static int SCAN(int [] requests, int head) {
     //     int head_movements = 0;
     //     int position = 0;
@@ -109,15 +132,21 @@ public class DiskSchedule {
 
     //     //iterate backwards to get the remaining requests (left side)
     //     for(int i = position - 1; i >= 0; i--) {
+    //     	System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
     //         head_movements += Math.abs(temp.get(i) - head);
     //         head = temp.get(i);
     //     }
 
     //     head_movements += 0 + head;
+    //     System.out.print("(|" + max + " - " + head + "|) + ");
     //     head = 0;
 
     //     //going to the right side first
     //     for(int i = position; i < temp.size(); i++) {
+    //     	if(i == 0)
+    //     		System.out.print("(|" + head + " - " + temp.get(i) + "|)");
+    //     	else
+    //     		System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
     //         head_movements += Math.abs(temp.get(i) - head);
     //         head = temp.get(i);
     //     }
@@ -125,7 +154,7 @@ public class DiskSchedule {
     //     return head_movements;
     // }
 
-    public static int C_SCAN(int[] requests, int head) {
+    public static int C_SCAN(int[] requests, int head, int max) {
         int head_movements = 0;
         int position = 0;
         ArrayList <Integer> temp = new ArrayList<Integer>();
@@ -145,20 +174,27 @@ public class DiskSchedule {
 
         //going to the right side first
         for(int i = position; i < temp.size(); i++) {
+        	System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
             head_movements += Math.abs(temp.get(i) - head);
             head = temp.get(i);
         }
 
-        head_movements += 4999 - head;
+        head_movements += max - head;
+        System.out.print("(|" + max + " - " + head + "|) + ");
         head = 0;
-        head_movements += 4999 - head;
+        head_movements += max - head;
+        System.out.print("(|" + max + " - " + head + "|) + ");
 
         //going to the right side again
         for(int i = 0; i < position; i++) {
+        	if(i == position - 1)
+        		System.out.print("(|" + head + " - " + temp.get(i) + "|)");
+        	else
+        		System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
             head_movements += Math.abs(temp.get(i) - head);
             head = temp.get(i);
         }
-
+        System.out.println();
         return head_movements;
     }
 
@@ -183,15 +219,20 @@ public class DiskSchedule {
 
         //going to the right side first
         for(int i = position; i < temp.size(); i++) {
+        	System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
             head_movements += Math.abs(temp.get(i) - head);
             head = temp.get(i);
         }
 
         //going to the left side now
         for(int i = position - 1; i >= 0; i--) {
+        	if(i == 0)
+        		System.out.print("(|" + head + " - " + temp.get(i) + "|)");
+        	else
+        		System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
             head_movements += Math.abs(temp.get(i) - head);
         }
-
+        System.out.println();
         return head_movements;
     }
 
@@ -216,6 +257,7 @@ public class DiskSchedule {
 
         //going to the right side first
         for(int i = position; i < temp.size(); i++) {
+        	System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
             head_movements += Math.abs(temp.get(i) - head);
             head = temp.get(i);
         }
@@ -224,22 +266,19 @@ public class DiskSchedule {
 
         //going to the right side again
         for(int i = 0; i < position; i++) {
+        	if(i == position - 1)
+        		System.out.print("(|" + head + " - " + temp.get(i) + "|)");
+        	else
+        		System.out.print("(|" + head + " - " + temp.get(i) + "|) + ");
             head_movements += Math.abs(temp.get(i) - head);
             head = temp.get(i);
         }
 
+        System.out.println();
         return head_movements;
 
     }
 }
 	
-
-
-
-
-
-
-
-
 
 
